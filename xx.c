a72d85824452dde50d30525c3e9f9e5779b9d99b
tony199600 <46002663+tony199600@users.noreply.github.com>
2018-12-20 00:07:58 +0800

    Add files via upload

---
+++ xx.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+printf("input your:");
+printf("1:show table list");
+printf(" ");
+int no,xx;
+main(){
+display_datain();
+display_inf();
+display_make();
+system("pause");
+}
+
